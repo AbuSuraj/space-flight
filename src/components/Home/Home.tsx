0be7f45c59a803a090674fab6c58caa4982091b8
Abu Suraj <abusurajbd@gmail.com>
2023-10-18 19:41:37 +0600

    add heading text

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Home.scss'
+const Home = () => {
+    return (
+        <div>
+            <section>
+                <div className='header-container'>
+                    <h1 className='font-medium'>Spaceflight details</h1>
+                    <p>Find out the elaborate features of all the past big spaceflights.</p>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
