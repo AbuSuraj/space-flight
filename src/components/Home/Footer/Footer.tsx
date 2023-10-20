0354597213debdca432bbced1c9a071f6b243bae
Abu Suraj <abusurajbd@gmail.com>
2023-10-20 15:17:31 +0600

    add footer

---
+++ src/components/Home/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <p className='text-base font-normal font-[Barlow] text-gray-700'>Created by the brilliant minds behind SpaceX</p>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.scss";
 import FlightDetails from "./Flight-Details/FlightDetails";
+import Footer from "./Footer/Footer";
 const Home = () => {
   return (
     <div>
@@ -83,6 +84,7 @@ const Home = () => {
       <section className="mt-16 ">
         <FlightDetails></FlightDetails>
       </section>
+      <section className="mt-[80.8px] mb-6"><Footer></Footer></section>
     </div>
   );
 };
