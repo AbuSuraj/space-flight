a6fb3712e80d7efe53b542097865c749b322d82c
Abu Suraj <abusurajbd@gmail.com>
2023-10-22 19:35:52 +0600

    remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-// import { useState } from 'react'
+
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import './App.scss'
 import Home from './components/Home/Home'
 
 function App() {
-  // const [count, setCount] = useState(0);
-
+  
   return (
     <>
-      {/* <Home></Home> */}
       <Router>
       <Routes>
      <Route path="" element={<Home />} />
